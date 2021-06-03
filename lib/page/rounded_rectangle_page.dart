6a586cd7460438d6dc97c4de5e1cb68af35d9a56
xrstudio <rencymalaviya@gmail.com>
2021-06-03 16:41:15 +0530

    Update rounded_rectangle_page.dart

--- lib/page/rounded_rectangle_page.dart
+++ lib/page/rounded_rectangle_page.dart
@@ -4,13 +4,13 @@ class RoundedRectanglePaintPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Center(
-          child: Container(
-            color: Colors.white,
-            width: 300,
-            height: 300,
-            child: CustomPaint(
-              painter: RoundedRectanglePainter(),
+          child: CustomPaint(
+
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100,vertical: 50),
+              child: Text("Submit",style: TextStyle(color: Colors.white),),
             ),
+            painter: RoundedRectanglePainter(),
           ),
         ),
       );
@@ -21,13 +21,13 @@ class RoundedRectanglePainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
       ..color = Colors.blue
-      ..strokeWidth = 10
+      ..strokeWidth = 3
       ..style = PaintingStyle.stroke;
 
     final a = Offset(size.width * 1 / 6, size.height * 1 / 4);
     final b = Offset(size.width * 5 / 6, size.height * 3 / 4);
     final rect = Rect.fromPoints(a, b);
-    final radius = Radius.circular(32);
+    final radius = Radius.circular(10);
 
     canvas.drawRRect(RRect.fromRectAndRadius(rect, radius), paint);
   }
